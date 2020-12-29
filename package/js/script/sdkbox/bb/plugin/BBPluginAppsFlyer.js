b76898814a0f33bf1000ff47601a86c2e3538310
hugo <hugohuang1111@gmail.com>
2020-12-29 18:22:25 +0800

    add patch file

--- package/js/script/sdkbox/bb/plugin/BBPluginAppsFlyer.js
+++ package/js/script/sdkbox/bb/plugin/BBPluginAppsFlyer.js
@@ -5,21 +5,21 @@ sdkbox.bb.plugin = sdkbox.bb.plugin || {};
 if (!sdkbox.bb.plugin.AppsFlyer) {
 
 let Msg = sdkbox.bb.Msg;
-let AppsFlyer = {}
+let BBPluginAppsFlyer = {}
 
 // const sys = sys || cc.sys;
 
-AppsFlyer.init = function() {
+BBPluginAppsFlyer.init = function() {
     sdkbox.bb.Bridge.init();
 
-    if (sys.os === sys.OS_ANDROID) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         sdkbox.bb.Bridge.addPlugin("AppsFlyer", "com.sdkbox.bb.bridge.plugin.appsflyer.PluginAppsFlyer");
-    } else if (sys.os === sys.OS_IOS) {
+    } else if (BBPluginAppsFlyer._isIOS()) {
         sdkbox.bb.Bridge.addPlugin("AppsFlyer", "BBPluginAppsFlyer");
     }
 }
 
-AppsFlyer.setListener = function(lis) {
+BBPluginAppsFlyer.setListener = function(lis) {
     if(!lis) {
         return
     }
@@ -31,78 +31,78 @@ AppsFlyer.setListener = function(lis) {
     })
 }
 
-AppsFlyer.setAppID = function(appID) {
+BBPluginAppsFlyer.setAppID = function(appID) {
     const m = new Msg("AppsFlyer", "setAppID")
     m.pushValue(appID)
 
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.setDevKey = function(devKey) {
+BBPluginAppsFlyer.setDevKey = function(devKey) {
     const m = new Msg("AppsFlyer", "setDevKey")
     m.pushValue(devKey)
 
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.setDebug = function(b) {
+BBPluginAppsFlyer.setDebug = function(b) {
     const m = new Msg("AppsFlyer", "setDebug");
     m.pushValue(b);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.setAdditionalData = function(json) {
+BBPluginAppsFlyer.setAdditionalData = function(json) {
     const m = new Msg("AppsFlyer", "setAdditionalData");
     m.pushValue(json);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.getAppsFlyerUID = function(cb) {
+BBPluginAppsFlyer.getAppsFlyerUID = function(cb) {
     const m = new Msg("AppsFlyer", "getAppsFlyerUID");
     sdkbox.bb.Bridge.send(m, function(msg) {
         cb && cb(msg.values[0]);
     });
 }
 
-AppsFlyer.setCustomerUserID = function(userID) {
+BBPluginAppsFlyer.setCustomerUserID = function(userID) {
     const m = new Msg("AppsFlyer", "setCustomerUserID");
     m.pushValue(userID);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.anonymizeUser = function(b) {
+BBPluginAppsFlyer.anonymizeUser = function(b) {
     const m = new Msg("AppsFlyer", "anonymizeUser");
     m.pushValue(b);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.collectASA = function(b) {
-    if (sys.os === sys.OS_IOS) {
+BBPluginAppsFlyer.collectASA = function(b) {
+    if (BBPluginAppsFlyer._isIOS()) {
         const m = new Msg("AppsFlyer", "collectASA");
         m.pushValue(b);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.waitForATTUserAuthorizationWithTimeoutInterval = function(seconds) {
-    if (sys.os === sys.OS_IOS) {
+BBPluginAppsFlyer.waitForATTUserAuthorizationWithTimeoutInterval = function(seconds) {
+    if (BBPluginAppsFlyer._isIOS()) {
         const m = new Msg("AppsFlyer", "waitForATTUserAuthorizationWithTimeoutInterval");
         m.pushValue(seconds);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.stop = function() {
+BBPluginAppsFlyer.stop = function() {
     const m = new Msg("AppsFlyer", "stop");
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.start = function() {
+BBPluginAppsFlyer.start = function() {
     const m = new Msg("AppsFlyer", "start");
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.logEvent = function(evt, json, cb) {
+BBPluginAppsFlyer.logEvent = function(evt, json, cb) {
     const m = new Msg("AppsFlyer", "logEvent");
     m.pushValue(evt);
     m.pushValue(json);
@@ -111,27 +111,27 @@ AppsFlyer.logEvent = function(evt, json, cb) {
     });
 }
 
-AppsFlyer.useReceiptValidationSandbox = function(b) {
-    if (sys.os === sys.OS_IOS) {
+BBPluginAppsFlyer.useReceiptValidationSandbox = function(b) {
+    if (BBPluginAppsFlyer._isIOS()) {
         const m = new Msg("AppsFlyer", "useReceiptValidationSandbox");
         m.pushValue(b);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.validateAndLogInAppPurchase = function(purchaseJson) {
+BBPluginAppsFlyer.validateAndLogInAppPurchase = function(purchaseJson) {
     const m = new Msg("AppsFlyer", "validateAndLogInAppPurchase");
     m.pushValue(purchaseJson);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.setMinTimeBetweenSessions = function(seconds) {
+BBPluginAppsFlyer.setMinTimeBetweenSessions = function(seconds) {
     const m = new Msg("AppsFlyer", "setMinTimeBetweenSessions");
     m.pushValue(seconds);
     sdkbox.bb.Bridge.send(m);
 }
 
-AppsFlyer.setResolveDeepLinkURLs = function(urls) {
+BBPluginAppsFlyer.setResolveDeepLinkURLs = function(urls) {
     const m = new Msg("AppsFlyer", "setResolveDeepLinkURLs")
     for(let i = 0; i < urls.length; i++) {
         m.pushValue(urls[i])
@@ -139,46 +139,73 @@ AppsFlyer.setResolveDeepLinkURLs = function(urls) {
     sdkbox.bb.Bridge.send(m)
 }
 
-AppsFlyer.setImeiData = function(data) {
-    if (sys.os === sys.OS_ANDROID) {
+BBPluginAppsFlyer.setImeiData = function(data) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         const m = new Msg("AppsFlyer", "setImeiData");
         m.pushValue(data);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.setAndroidIdData = function(data) {
-    if (sys.os === sys.OS_ANDROID) {
+BBPluginAppsFlyer.setAndroidIdData = function(data) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         const m = new Msg("AppsFlyer", "setAndroidIdData");
         m.pushValue(data);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.setCollectIMEI = function(b) {
-    if (sys.os === sys.OS_ANDROID) {
+BBPluginAppsFlyer.setCollectIMEI = function(b) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         const m = new Msg("AppsFlyer", "setCollectIMEI");
         m.pushValue(b);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.setCollectAndroidID = function(b) {
-    if (sys.os === sys.OS_ANDROID) {
+BBPluginAppsFlyer.setCollectAndroidID = function(b) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         const m = new Msg("AppsFlyer", "setCollectAndroidID");
         m.pushValue(b);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-AppsFlyer.setCollectOaid = function(b) {
-    if (sys.os === sys.OS_ANDROID) {
+BBPluginAppsFlyer.setCollectOaid = function(b) {
+    if (BBPluginAppsFlyer._isAndroid()) {
         const m = new Msg("AppsFlyer", "setCollectOaid");
         m.pushValue(b);
         sdkbox.bb.Bridge.send(m);
     }
 }
 
-sdkbox.bb.plugin.AppsFlyer = AppsFlyer;
+BBPluginAppsFlyer._isAndroid = function() {
+    if (cc) {
+        if (cc.sys.platform == cc.sys.ANDROID) {
+            return true;
+        }
+    } else {
+        if (sys.os === sys.OS_ANDROID) {
+            return true;
+        }
+    }
+    return false;
+}
+
+BBPluginAppsFlyer._isIOS = function() {
+    if (cc) {
+        if (cc.sys.platform == cc.sys.IPHONE || cc.sys.platform == cc.sys.IPAD) {
+            return true;
+        }
+    } else {
+        if (sys.os === sys.OS_IOS) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+sdkbox.bb.plugin.AppsFlyer = BBPluginAppsFlyer;
 
 }
